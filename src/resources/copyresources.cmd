14fd53196d27e7235d4a5746ce3c93a46c5b32e6
Yacow <midnight.howlet@gmail.com>
2017-08-25 17:24:21 +0300

    icons

--- src/resources/copyresources.cmd
+++ src/resources/copyresources.cmd
@@ -1,12 +1,16 @@
 @echo off
 set out-assets-path=.\..\..\build\assets
 set out-sound-path=.\..\..\build\sound
+set out-icon-path=.\..\..\build\icon
 
 if exist %out-assets-path% rmdir /s /q %out-assets-path%
 mkdir %out-assets-path%
 
 if exist %out-sound-path% rmdir /s /q %out-sound-path%
 mkdir %out-sound-path%
+
+if exist %out-icon-path% rmdir /s /q %out-icon-path%
+mkdir %out-icon-path%
 
 setlocal EnableDelayedExpansion
 for /d %%X in (%out-assets-path%) do (
@@ -27,7 +31,17 @@ for /d %%X in (%out-sound-path%) do (
     popd
 )
 
+for /d %%X in (%out-icon-path%) do (
+    set out-path=%%~fX
+    pushd icon
+    for /r %%I in (*.png) do (
+        copy /y %%~fI !out-path! > nul
+    )
+    popd
+)
+
 set out-path=
 set out-assets-path=
 set out-sound-path=
+set out-icon-path=
 @echo on
